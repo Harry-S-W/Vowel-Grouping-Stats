46e21f05892b902e59b3cd5afc3b38dfd55b0e29
harrywoodhouse <woodhouses.harry@gmail.com>
2025-11-17 18:27:55 +0000

    Merging data

--- analysis.R
+++ analysis.R
@@ -14,10 +14,10 @@ explode_groups <- function(df, group_col = "GROUP"){
     # take all the vals in group column and turn them into lists called sound
     mutate(SOUND = str_split(GROUP, ",")) %>%
     
-    # make each item of the list sound into a seperate row
+    # make each item of the list into a seperate row
     unnest(SOUND) %>%
     
-    # making sure to remove white space
+    # remove white space around the strs
     mutate(SOUND = str_trim(SOUND)) %>%
     
     # Drop empty strings from trailing commas
@@ -25,16 +25,48 @@ explode_groups <- function(df, group_col = "GROUP"){
     
     # Convert str to int
     mutate(SOUND = as.numeric(SOUND))
+
 }
 
 #head(vowels_long, 50)
 vowels_long <- explode_groups(vowels)
-print(vowels_long, n = 50)
+# we can export this as a csv so we can refer to it later without reusing this function
+
+write.csv(vowels_long, "data/exploded_data.csv")
+#print(vowels_long, n = 10)
 
 
 # Now we need to associate numbers in the exploded data to the vowels in the 
 # stimulus excel. 
 
-stimulus_mergine <- function(participant_data, stimulus_data, group_col = "SOUND"){
-  print("placeholder")
+stimulus_merging <- function(participant_data, stimulus_data, group_col = "SOUND"){
+  # we will look at each group and next to each number, put its corresponding stimulus
+  # name so we can better look at how characteristics of sounds may have influenced
+  # grouping. We will also have a boolean column for if the sound is palatalized or not
+  # as well as female or male speaking column
+  
+  sound_data <- read_csv(participant_data)
+  stimuli_data <- read_csv(stimulus_data)
+  
+  merged_data <- sound_data %>%
+    left_join(
+      stimuli_data,
+      by = c(
+        "SLIDE" = "Aslide",
+        "SOUND" = "IconNumber"
+      )
+    )
+  
+  write.csv(merged_data, participant_data)
+  
+  # if we convert all the group numbers into a list, and then iterate through that list and make a list for each number
+  # which includes the data from stimuli data, we can then convert those lists to hav each item on its own column
+  # in the exploded data csv file
+  
+  
+  
+  print(merged_data)
 }
+
+stim_merge_test <- stimulus_merging("data/exploded_data.csv", "data/Stimuli.xlsx - Lookup Matrix.csv")
+# print(stim_merge_test)
