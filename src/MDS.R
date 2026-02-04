dd3824560f49c1621c9ee5198203e4a4db6a79cd
harrywoodhouse <woodhouses.harry@gmail.com>
2026-02-04 19:31:07 +0000

    Removed code duplication

--- src/MDS.R
+++ src/MDS.R
@@ -1,15 +1,23 @@
 # FILE PULLS CODE FROM DANIEL DAIDONES WEBSITE:
 # https://www.ddaidone.com/uploads/1/0/5/2/105292729/mds_analysis.rmd
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+base_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(base_path)
+
+data_folder <- file.path(base_path, "../data")
+output_folder <- file.path(base_path, "../data_output")
+
+if (!dir.exists(data_folder)) dir.create(data_folder)
+if (!dir.exists(output_folder)) dir.create(output_folder)
+
 source("DATA_CONVERSION.R")
 
-# converting the csv data to a dissimilarity matrix 
-my_matrix <- run_conversion("../data/speaker_data.csv", "../data/stimuli.csv", output_path = "../data/FCmatrix.txt")
+# We call the DATA_CONVERTER.R file to turn our csv datasets into dissimilarity matrixes
+matrix_path <- file.path(data_folder, "FCmatrix.txt")
+my_matrix <- run_conversion("../data/speaker_data.csv", "../data/stimuli.csv", output_path = matrix_path)
 
-# downloading daniel daidones code from her website. 
-# please email jgf526@york.ac.uk if this link stops working and I will try to find a new one
-# I am not claiming ownership but I also won't put someone else email in a git repo
+# Downloading daidones code
+# if this link stops working, email me at jgf526@york.ac.uk
 
 daidone_url <- "https://www.ddaidone.com/uploads/1/0/5/2/105292729/mds_analysis.rmd"
 raw_file <- "daidone_raw.Rmd"
@@ -22,47 +30,43 @@ if (!file.exists(raw_file)) {
 
 content <- readLines(raw_file)
 
-# Because Daidones code has some hard coded paths we have to comment them out
+# Comment out hardcoded root directory
 content <- gsub("knitr::opts_knit\\$set\\(root.dir", "# knitr::opts_knit\\$set\\(root.dir", content)
 
-# Similar to above but now we are changing the hard coded matrix path to our matrix path
+# We then change the paths in daidones file to ours
 content <- gsub('read.table\\("Output_Matrix_AllContexts_percent_dis.txt"', 
-                'read.table("../data/FCmatrix.txt"', content)
+                paste0('read.table("', matrix_path, '"'), content)
 
-content <- gsub('FCmatrixAll=read.table', 
-                'FCmatrixAll_raw=read.table', content)
-
+content <- gsub('FCmatrixAll=read.table', 'FCmatrixAll_raw=read.table', content)
 fix_line <- "FCmatrixAll <- as.matrix(FCmatrixAll_raw[, sapply(FCmatrixAll_raw, is.numeric)]); FCmatrixAll[FCmatrixAll < 0] <- 0; diag(FCmatrixAll) <- 0"
 content <- append(content, fix_line, after = grep("FCmatrixAll_raw=read.table", content))
 
 writeLines(content, clean_file)
 
-rmarkdown::render(clean_file, output_format = "html_document")
-
-# All outputs go to data_output 
-# if there isn't one, it makes one automatically. 
-
-output_folder <- "../data_output"
-if (!dir.exists(output_folder)) dir.create(output_folder)
-
 rmarkdown::render(
   input = clean_file, 
   output_format = "html_document",
-  output_dir = output_folder,        # Directs the HTML file
-  intermediates_dir = output_folder   # Directs the 'hidden' math files
+  output_dir = output_folder,
+  clean = TRUE 
 )
 
-if (file.exists("FCmatrix.txt")) {
-  file.copy("FCmatrix.txt", "../data/FCmatrix.txt", overwrite = TRUE)
+# moving dimension plots to /data_output before we purge unwanted files in /src
+all_files_in_src <- list.files(base_path)
+rescues <- all_files_in_src[grepl("\\.(png|txt|csv)$", all_files_in_src)]
+rescues <- rescues[!rescues %in% c(raw_file, clean_file, "FCmatrix.txt", "DATA_CONVERSION.R", "MDS.R")]
+
+if (length(rescues) > 0) {
+  file.copy(file.path(base_path, rescues), file.path(output_folder, rescues), overwrite = TRUE)
+  file.remove(file.path(base_path, rescues))
 }
 
-if (file.exists("../data/FCmatrix.txt")) {
-  file.copy("../data/FCmatrix.txt", file.path(output_folder, "FCmatrix_Final.txt"), overwrite = TRUE)
+final_files_in_src <- list.files(base_path)
+keep_these <- c("MDS.R", "DATA_CONVERSION.R", "daidone_raw.Rmd", "daidone_logic.Rmd")
+to_delete <- final_files_in_src[!(final_files_in_src %in% keep_these)]
+
+for(f in to_delete) {
+  target <- file.path(base_path, f)
+  if(!dir.exists(target)) file.remove(target)
 }
 
-# Just trying to remove any extra files that might stay in /src which should only contain source code
-survivors <- list.files(pattern = ".*\\.(png|txt|csv)$")
-if (length(survivors) > 0) {
-  file.copy(survivors, file.path(output_folder, survivors))
-  file.remove(survivors)
-}
+message("Processing complete")
