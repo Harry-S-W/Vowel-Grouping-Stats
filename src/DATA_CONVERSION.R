b86c55f3fca26d137abc058234db6861ea6482c0
harrywoodhouse <woodhouses.harry@gmail.com>
2026-02-04 18:21:43 +0000

    Working prototype

--- src/DATA_CONVERSION.R
+++ src/DATA_CONVERSION.R
@@ -1,50 +1,53 @@
-#---
-# title: "DATA CONVERSION"
-# author: "Harry Woodhouse"
-# date: "`04/02/2026`"
-# output: html_document
-# license: "GPLv3
-
-
-# DATA_CONVERSION.R
-# Takes a standard CSV and tursn it into a Dissimilarity Matrix
-# Exported to "data/FCmatrix.txt"
-# ---
-
-library(tidyverse)
-
-run_conversion <- function(raw_csv, stimuli_csv, output_path = "data/FCmatrix.txt") {
+run_conversion <- function(raw_csv, stimuli_csv, output_path = "../data/FCmatrix.txt") {
   
   raw_data <- read.csv(raw_csv)
   stimuli  <- read.csv(stimuli_csv)
   
-  n_participants <- length(unique(raw_data$PARTICIPANT))
+  # 1. Dynamically count participants to avoid negative distances
+  unique_ppts <- unique(raw_data$PARTICIPANT)
+  n_participants <- length(unique_ppts)
+  
+  # Initialize matrix (40x40)
   sim_matrix <- matrix(0, nrow = 40, ncol = 40)
   
   for(i in 1:nrow(raw_data)) {
-    offset <- if(raw_data$SLIDE[i] == 1) 0 else 20
-    items <- as.numeric(unlist(strsplit(as.character(raw_data$GROUP[i]), "[, ]+")))
-    items <- items[!is.na(items)]
+    # Clean the GROUP string before splitting to avoid NAs
+    clean_group <- gsub("[^0-9, ]", "", as.character(raw_data$GROUP[i]))
+    items <- as.numeric(unlist(strsplit(clean_group, "[, ]+")))
+    items <- items[!is.na(items) & items > 0 & items <= 20] # Bounds check
     
     if(length(items) > 1) {
+      offset <- if(raw_data$SLIDE[i] == 1) 0 else 20
       adj_items <- items + offset
-      pairs <- combn(sort(adj_items), 2)
-      for(j in 1:ncol(pairs)) {
-        sim_matrix[pairs[1,j], pairs[2,j]] <- sim_matrix[pairs[1,j], pairs[2,j]] + 1
-        sim_matrix[pairs[2,j], pairs[1,j]] <- sim_matrix[pairs[2,j], pairs[1,j]] + 1
+      
+      # Ensure we don't exceed matrix bounds (1-40)
+      adj_items <- adj_items[adj_items >= 1 & adj_items <= 40]
+      
+      if(length(adj_items) > 1) {
+        pairs <- combn(sort(adj_items), 2)
+        for(j in 1:ncol(pairs)) {
+          sim_matrix[pairs[1,j], pairs[2,j]] <- sim_matrix[pairs[1,j], pairs[2,j]] + 1
+          sim_matrix[pairs[2,j], pairs[1,j]] <- sim_matrix[pairs[2,j], pairs[1,j]] + 1
+        }
       }
     }
   }
   
+  # 2. Convert to Dissimilarity (Distance)
   dissim_matrix <- n_participants - sim_matrix
+  
+  # 3. Safety Floor: Distances cannot be negative or NA
+  dissim_matrix[dissim_matrix < 0] <- 0
   diag(dissim_matrix) <- 0
   
-  # label adding from stimuli csv :)
-  labels <- stimuli$FileName[1:40]
+  # 4. Apply Labels
+  labels <- as.character(stimuli$FileName[1:40])
   colnames(dissim_matrix) <- labels
   rownames(dissim_matrix) <- labels
   
-  write.table(dissim_matrix, file = output_path, sep = "\t", quote = FALSE, col.names = NA)
+  # 5. Export CLEANly (Standard tab-separated format)
+  # We avoid 'col.names = NA' to keep the header simple for Danielle's script
+  write.table(dissim_matrix, file = output_path, sep = "\t", quote = FALSE, col.names = TRUE, row.names = TRUE)
   
   return(dissim_matrix)
 }
--- src/MDS.R
+++ src/MDS.R
@@ -1,19 +1,28 @@
 # FILE PULLS CODE FROM DANIEL DAIDONES WEBSITE:
 # https://www.ddaidone.com/uploads/1/0/5/2/105292729/mds_analysis.rmd
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # getting the data conversion file
-source("scripts/conversion_logic.R")
+source("DATA_CONVERSION.R")
 
 # running the conversion on the dataset - I will remove the hardcoded path soon
-my_matrix <- run_conversion("data/user_data.csv", "data/Stimuli.csv")
-
+my_matrix <- run_conversion("../data/speaker_data.csv", "../data/stimuli.csv", output_path = "../data/FCmatrix.txt")
 
 # To comply with personal copyright this code here downloads Daidones code externally
 daidone_url <- "https://www.ddaidone.com/uploads/1/0/5/2/105292729/mds_analysis.rmd"
-dest_file <- "scripts/daidone_logic.Rmd"
+dest_file <- "daidone_logic.Rmd"
 
 if (!file.exists(dest_file)) {
-  download.file(daidone_url, dest_file)
+  # We use 'method = "libcurl"' and custom headers to bypass the 403 error
+  download.file(
+    url = daidone_url, 
+    destfile = dest_file, 
+    method = "libcurl", 
+    headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/91.0.4472.124 Safari/537.36")
+  )
 }
 
+# Now run the render
 rmarkdown::render(dest_file, output_format = "html_document")
+
+
