32fbcad02aeb30cbf00140f97650506a4bbd4110
harrywoodhouse <woodhouses.harry@gmail.com>
2026-02-04 17:44:29 +0000

    DATA_CONVERSION logic

--- src/DATA_CONVERSION.R
+++ src/DATA_CONVERSION.R
@@ -1,23 +1,26 @@
----
-title: "DATA CONVERSION"
-author: "Harry Woodhouse"
-date: "`04/02/2026`"
-output: html_document
-license: "GPLv3
----
+#---
+# title: "DATA CONVERSION"
+# author: "Harry Woodhouse"
+# date: "`04/02/2026`"
+# output: html_document
+# license: "GPLv3
+
+
+# DATA_CONVERSION.R
+# Takes a standard CSV and tursn it into a Dissimilarity Matrix
+# Exported to "data/FCmatrix.txt"
+# ---
 
 library(tidyverse)
 
 run_conversion <- function(raw_csv, stimuli_csv, output_path = "data/FCmatrix.txt") {
-  # 1. Load data
+  
   raw_data <- read.csv(raw_csv)
   stimuli  <- read.csv(stimuli_csv)
   
-  # 2. Setup (Logic for 40 items across 2 slides)
   n_participants <- length(unique(raw_data$PARTICIPANT))
   sim_matrix <- matrix(0, nrow = 40, ncol = 40)
   
-  # 3. "Explode" the grouping strings
   for(i in 1:nrow(raw_data)) {
     offset <- if(raw_data$SLIDE[i] == 1) 0 else 20
     items <- as.numeric(unlist(strsplit(as.character(raw_data$GROUP[i]), "[, ]+")))
@@ -33,16 +36,14 @@ run_conversion <- function(raw_csv, stimuli_csv, output_path = "data/FCmatrix.tx
     }
   }
   
-  # 4. Convert Similarity to Dissimilarity
   dissim_matrix <- n_participants - sim_matrix
   diag(dissim_matrix) <- 0
   
-  # 5. Add labels from stimuli sheet
+  # label adding from stimuli csv :)
   labels <- stimuli$FileName[1:40]
   colnames(dissim_matrix) <- labels
   rownames(dissim_matrix) <- labels
   
-  # 6. Save for Danielle's script to find
   write.table(dissim_matrix, file = output_path, sep = "\t", quote = FALSE, col.names = NA)
   
   return(dissim_matrix)
